c47d701e3831e9f771f682b8111806a0dafc5dcc
SyedShafkatulHassan <87982712+SyedShafkatulHassan@users.noreply.github.com>
2022-12-18 02:31:27 +0600

    Create A.Shafkat2.cpp

---
+++ A.Shafkat2.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+struct process
+{
+    int ProcessID;
+    int Arivaltime;
+    int BirstTime;
+    int TrunAroundTime;
+    int WaitingTime;
+    int ResponseTime;
+    int complissionTime;
+} ;
+
+bool compare(process a, process b)
+{
+    return a.BirstTime< b.BirstTime;
+}
+
+int main()
+{
+    int no_of_process;
+    cout << "Enter the number of process: ";
+    cin >> no_of_process;
+    vector<process> p(no_of_process);
+    for (int i = 0; i < no_of_process; i++)
+    {
+        cout << "Enter the arrival time and burst time of process " << i + 1 << ": ";
+        cin >> p[i].Arivaltime >> p[i].BirstTime;
+        p[i].ProcessID = i + 1;
+    }
+    sort(p.begin(), p.end(), compare);
+    int CurentTime = 0;
+    for (int i = 0; i < no_of_process; i++)
+    {
+        if (CurentTime < p[i].Arivaltime)
+        {
+            CurentTime = p[i].Arivaltime;
+        }
+        p[i].ResponseTime = CurentTime-p[i].Arivaltime;
+        CurentTime = CurentTime+p[i].BirstTime;
+        p[i].TrunAroundTime = CurentTime - p[i].Arivaltime;
+        p[i].WaitingTime = p[i].TrunAroundTime - p[i].BirstTime;
+        p[i].complissionTime=CurentTime;
+    }
+    cout << "Process\tArivalTime BirstTime  ResponseTime  TrunAroundTime  WaitingTime  ComplessionTime\n";
+    for (int i = 0; i < no_of_process; i++)
+    {
+        cout << p[i].ProcessID << "\t\t" << p[i].Arivaltime << "\t" << p[i].BirstTime << "\t" << p[i].ResponseTime << "\t\t" << p[i].TrunAroundTime << "\t\t" << p[i].WaitingTime <<"\t"<<p[i].complissionTime <<"\n";
+    }
+
+}
+
